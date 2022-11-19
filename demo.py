289a06ac279a66fb8bfc98a05b8af6c60aa8e149
Vincent Ho <tsz@student.ubc.ca>
2022-11-18 20:25:45 -0800

    q9

--- demo.py
+++ demo.py
@@ -13,5 +13,11 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
+
+def main(arg1, arg2, arg3, arg4):
+    print(opt)
+    print(type(opt))
+    
+    
+if __name__ == "__main__":
+    main(opt["arg1"], opt["--arg2"], opt["--arg3"], opt["--arg4"])
